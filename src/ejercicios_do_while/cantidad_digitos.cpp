eed331721b425820f40aa1aa3f7c90e794e9c0c0
PsyQox <luisrodrizza@gmail.com>
2025-07-22 13:24:34 -0500

    padespues

--- src/ejercicios_do_while/cantidad_digitos.cpp
+++ src/ejercicios_do_while/cantidad_digitos.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     int numero, acumulador;
     numero = 10;
     acumulador = 1;
+
     do
     {
         /* code */
-        acumulador ++;
-        cout << "Hola " << acumulador << endl;
+
     } while (acumulador <= numero);
     
     return 0;
