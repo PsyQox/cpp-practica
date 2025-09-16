426276a41f699967ea7ab246c71312335c460a02
PsyQox <luisrodrizza@gmail.com>
2025-09-16 14:46:26 -0500

    argumentos

---
+++ funciones/funciones-argumentos-predeterminados.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+//los argumentos predeterminados se utilizan cuando no se proporcionana argumentos o solo unos pocos argumentos llaman a la funcion.
+
+
+int main() {
+
+}
