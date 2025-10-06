c2d0aaea7a3c694ce0f795e73c89525b08d1ff18
PsyQox <luisrodrizza@gmail.com>
2025-10-06 10:37:27 -0500

    asda

--- funciones/funciones-generar-matrices-multidimencionales.cpp
+++ funciones/funciones-generar-matrices-multidimencionales.cpp
@@ -4,7 +4,10 @@ using namespace std;
 int main(){
     // Matrices Multidimencionales
     // Array de manera avanzada - array - arreglo - cadena - matriz unidimencional
-
+    // matris bidimencional - 2 dimenciones
+    // int a [][]
+    // matris multidimencional - 3 dimenciones
+    // int a [][][]
 
 
     return 0;
